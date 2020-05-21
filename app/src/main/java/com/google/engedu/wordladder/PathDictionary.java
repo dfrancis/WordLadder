f2699a6bc9d277d0974a5740ee5ba1d9a84e8eec
David Francis <dfrancis22@gmail.com>
2020-05-21 15:32:10 -0700

    Milestone 1

--- app/src/main/java/com/google/engedu/wordladder/PathDictionary.java
+++ app/src/main/java/com/google/engedu/wordladder/PathDictionary.java
@@ -26,26 +26,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Queue;
 
 public class PathDictionary {
     private static final int MAX_WORD_LENGTH = 4;
+    private static final int MAX_SEARCH_DEPTH = 5;
     private static HashSet<String> words = new HashSet<>();
+    private static HashMap<String, ArrayList<String>> wordGraph = new HashMap<>();
+
+    private boolean isNeighbourString(String a, String b) {
+        if ((a == null) || (b == null) || (a.length() == 0) || (b.length() == 0) || (a.length() != b.length()) || (a.compareTo(b) == 0)) {
+            return false;
+        }
+
+        boolean retval = false;
+        int diffCount = 0;
+        for (int idx = 0; idx < a.length(); ++idx) {
+            if (a.charAt(idx) != b.charAt(idx)) {
+                if (++diffCount > 1) {
+                    break;
+                }
+            }
+        }
+
+        if (diffCount == 1) {
+            retval = true;
+        }
+        return retval;
+    }
 
     public PathDictionary(InputStream inputStream) throws IOException {
         if (inputStream == null) {
             return;
         }
-        Log.i("Word ladder", "Loading dict");
+        Log.d("Word ladder", "Loading dict");
         BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
         String line = null;
-        Log.i("Word ladder", "Loading dict");
+        Log.d("Word ladder", "Loading dict");
         while((line = in.readLine()) != null) {
             String word = line.trim();
             if (word.length() > MAX_WORD_LENGTH) {
                 continue;
             }
             words.add(word);
+            wordGraph.put(word, new ArrayList<String>());
+        }
+        Iterator<String> iter = words.iterator();
+        while (iter.hasNext()) {
+            String vertexString = iter.next();
+            Iterator<String> iter2 = words.iterator();
+            while (iter2.hasNext()) {
+                String neighbourString = iter2.next();
+                if (isNeighbourString(vertexString, neighbourString)) {
+                    wordGraph.get(vertexString).add(neighbourString);
+                }
+            }
         }
     }
 
@@ -54,10 +90,51 @@ public class PathDictionary {
     }
 
     private ArrayList<String> neighbours(String word) {
-        return new ArrayList<String>();
+        ArrayList<String> nList = wordGraph.get(word);
+        if (nList == null) {
+            nList = new ArrayList<String>();
+        }
+        return nList;
     }
 
     public String[] findPath(String start, String end) {
+        ArrayList<String> path = new ArrayList<String>();
+        ArrayDeque<ArrayList<String>> pathsExplored = new ArrayDeque<>();
+
+        path.add(start);
+        pathsExplored.add(path);
+        while (! pathsExplored.isEmpty()) {
+            ArrayList<String> currPath = pathsExplored.removeFirst();
+            String currPathLogStr = new String();
+            for (int idx = 0; idx < currPath.size(); ++idx) {
+                currPathLogStr += currPath.get(idx) + " ";
+            }
+            Log.d("Word ladder", "currPath " + currPathLogStr);
+            ArrayList<String> neighbourStrings = neighbours(currPath.get(currPath.size() - 1));
+            for (int idx = 0; idx < neighbourStrings.size(); ++idx) {
+                String neighbour = neighbourStrings.get(idx);
+                if (neighbour.compareTo(end) == 0) {
+                    Log.d("Word ladder", "Found complete path");
+                    String[] retArray = new String[currPath.size()];
+                    retArray = currPath.toArray(retArray);
+                    return retArray;
+                }
+            }
+
+            if (currPath.size() >= MAX_SEARCH_DEPTH) {
+                continue;
+            }
+
+            for (int idx = 0; idx < neighbourStrings.size(); ++idx) {
+                String neighbour = neighbourStrings.get(idx);
+                ArrayList<String> newPath = null;
+                if (! currPath.contains(neighbour)) {
+                    newPath = new ArrayList<String>(currPath);
+                    newPath.add(neighbour);
+                    pathsExplored.add(newPath);
+                }
+            }
+        }
         return null;
     }
 }
